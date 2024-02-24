b855db044a7e0c00e2f5cd069bb23b1552ac5636
Himanshu Kumar Verma <76449638+Himanshuiiit@users.noreply.github.com>
2024-02-24 17:33:19 +0530

    Update AxiosInstance.ts

--- client/src/modules/shared/apis/AxiosInstance.ts
+++ client/src/modules/shared/apis/AxiosInstance.ts
@@ -4,5 +4,9 @@ const BASE_URL: string = import.meta.env.VITE_BASE_URL;
 
 export default axios.create({
   baseURL: BASE_URL,
-  withCredentials: true
+  withCredentials: true,
+  headers: {
+    'Access-Control-Allow-Origin' : '*',
+  'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+  }
 });
