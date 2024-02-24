2a6c88fb273d7869a58530786e53e40a088996c3
Himanshu Kumar Verma <76449638+Himanshuiiit@users.noreply.github.com>
2024-02-24 19:40:02 +0530

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.setGlobalPrefix('api');
   app.enableCors({
     origin: [process.env.FRONTEND_URL_DEV, process.env.FRONTEND_URL_PROD],
