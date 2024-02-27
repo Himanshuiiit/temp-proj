f6911f4e7fc2a74ef937937e3d201464d5ca01c3
Himanshu Verma <hims6482@gmail.com>
2024-02-27 12:22:28 +0530

    fix:localstorage bug

--- client/src/modules/core/pages/Application/Application.tsx
+++ client/src/modules/core/pages/Application/Application.tsx
@@ -216,6 +216,7 @@ const Application: React.FC = () => {
                   appId: id,
                   versionId: activeVersion?.id
                 });
+                localStorage.setItem('activeVerison', JSON.stringify({...activeVersion, released: true}));
                 navigate(`/release/${id}?versionId=${activeVersion?.id}`);
               }}
             >
